dd2619c9dcf32455d7b6542be538cc7365fe7dfe
Niteesh <104364679+GodOfZap@users.noreply.github.com>
2025-10-04 15:21:28 +0530

    Create Primality.py

---
+++ Ex5/Primality.py
@@ -0,0 +1,15 @@
+def primality_test(n):
+    if n <= 1:
+        return "Not Prime"
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return "Not Prime"
+    return "Prime"
+
+def main():
+    number = int(input("Enter number to test primality: "))
+    result = primality_test(number)
+    print("Primality Test Result:", result)
+
+if __name__ == "__main__":
+    main()
