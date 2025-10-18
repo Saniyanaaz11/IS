cfafb2028ce3c9d280aff81b5eae91b94216cb89
Niteesh <104364679+GodOfZap@users.noreply.github.com>
2025-10-18 15:57:50 +0530

    Example Usage
    Added Example Usage

--- Ex4/ECC.py
+++ Ex4/ECC.py
@@ -22,3 +22,15 @@ print("\nEncrypted Ciphertext (hex):", ciphertext_hex)
 # Step 4: Decrypt using private key
 decrypted = decrypt(private_key_hex, ciphertext)
 print("\nDecrypted Text:", decrypted.decode())
+##################################################
+# Example Output:
+#ECC Key Pair Generated (secp256k1)
+#Public Key (hex): 030ac1de3a90225837b140ee8c992e479b59cdd8cf4af8ad5106408a1335668d3b
+#Private Key (hex): 48eb02d2fc82b6c4e47d623634511fb081d916236dd9014766057e2f5ec5035c
+#
+#Enter plaintext to encrypt: ECC
+#
+#Encrypted Ciphertext (hex): 0456e4e4a91e43dfc760d2c9d284f7e03a2f110b27d3419f2b7e7c28e3bca6878f953d2e8c2e6959cc5545c91573f3e9a3456825e3b8ebbc4fb7ceba30450ccc18b8011f8f39a7d90576a2990cbe6883027ad6dd8e35030874b13bb210021e28554599fa
+#
+#Decrypted Text: ECC
+##################################################
