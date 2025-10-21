38f92a3f387e2b061a8dfcb83beddd35c1f4fd3e
Niteesh <104364679+GodOfZap@users.noreply.github.com>
2025-10-21 21:43:43 +0530

    Example Usage
    Added Example Usage

--- Ex5/Primality.py
+++ Ex5/Primality.py
@@ -13,3 +13,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+#################################################
+# Example Output:
+#Enter number to test primality: 29
+#Primality Test Result: Prime
+###################################################
