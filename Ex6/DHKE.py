931f644cc14b5f6963df6fd750cf3c1ef5eadbcf
Niteesh <104364679+GodOfZap@users.noreply.github.com>
2025-10-23 16:46:13 +0530

    Example Usage
    Added Example Usage

--- Ex6/DHKE.py
+++ Ex6/DHKE.py
@@ -19,3 +19,15 @@ print(f"\nAlice's public key: {A}")
 print(f"Bob's public key: {B}")
 print(f"Shared secret key (computed by Alice): {shared_secret_alice}")
 print(f"Shared secret key (computed by Bob): {shared_secret_bob}")
+#####################################################
+# Example Output:
+#Enter a prime number (p): 7
+#Enter a primitive root of 7 (g): 3
+#Enter private key for Alice: 5
+#Enter private key for Bob: 3
+#
+#Alice's public key: 5
+#Bob's public key: 6
+#Shared secret key (computed by Alice): 6
+#Shared secret key (computed by Bob): 6
+#####################################################
